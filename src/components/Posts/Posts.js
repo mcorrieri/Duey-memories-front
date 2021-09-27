b15dd741237bdf8e10f53d24a48fad6eb0a55ef4
Michael <Mlcorrieri@gmail.com>
2021-09-26 20:47:46 -0400

    redux added

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -2,8 +2,13 @@ import React from "react";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 
+import { useSelector } from "react-redux";
+
 function Posts() {
+  const posts = useSelector((state) => state.posts);
   const classes = useStyles();
+
+  console.log(posts);
   return (
     <div>
       <h1>POSTS</h1>
